a39b9470f31959996694e811074f5539e53db1d3
Kyle Hornacek <kylehornacek@Kyles-MacBook.local>
2020-10-19 16:04:44 -0500

    Login update Oct 19 2020 4:04PM

---
+++ find_element.php
@@ -0,0 +1,14 @@
+<?php
+$db = mysqli_connect('127.0.0.1', 'root', 'Kjh21498!', 'sys') or die('Error connecting to server');
+$email = mysqli_real_escape_string($db, $_REQUEST['email']);
+$password = mysqli_real_escape_string($db, $_REQUEST['password']);
+$sql = "SELECT * FROM email_login_table WHERE email = '$email' AND password = '$password'";
+$result = mysqli_query($db,$sql);
+$num_rows = mysqli_num_rows($result);
+
+if($num_rows == 1) {
+  header("Location: survey.html");
+} else {
+  header("Location: index.html");
+}
+ ?>
